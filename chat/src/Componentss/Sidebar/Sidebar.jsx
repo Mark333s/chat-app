28b78cc2f00628d5eafc25d0acec99dfac14cd79
Mark333s <markomelnychyn333@gmail.com>
2022-02-02 17:48:14 +0000

    add NavLink

--- chat/src/Componentss/Sidebar/Sidebar.jsx
+++ chat/src/Componentss/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from './Sidebar.module.css';
+import { NavLink } from "react-router-dom";
 
 
 const Sidebar =() => {
@@ -10,9 +11,9 @@ return(
 
   <div className={s.messages}>
 
-  <div className={s.item}><a href='/Josefina'>Josefina</a></div>
-  <div className={s.item}><a href='/Alica'>Alica</a></div>
-  <div className={s.item}><a href='/Velazquez'>Velazquez</a></div>
+  <div className={s.item}><NavLink to='/Josefina'>Josefina</NavLink></div>
+  <div className={s.item}><NavLink to='/Alica'>Alica</NavLink></div>
+  <div className={s.item}><NavLink to='/Velazquez'>Velazquez</NavLink></div>
 
 </div>
     </div>
